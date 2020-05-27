9fea225fa81cb63c0910a98ae18dd4ead73db18a
Heitor Polidoro <heitor.polidoro@gmail.com>
2020-05-27 13:57:42 -0300

    config links

--- create_links.sh
+++ create_links.sh
@@ -4,12 +4,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # hidden files
 cd ${DIR}
-cd home
+
 echo "Creating symbolic links in ~/"
+cd home
 for file in $(ls -dp .[^.]* | grep -v /); do
     ln -sfv ${DIR}/home/${file} ~/${file}
 done
-cd -
+cd - 1> /dev/null
 
 echo "Creating symbolic link ~/bin"
 ln -sfv ${DIR}/scripts ~/bin
+
+echo "Creating symbolic links for config"
+cd config
+# shellcheck disable=SC2045
+for dir in $(ls); do
+    ln -sfv ${DIR}/config/${dir} ~/.config/${dir}
+done
